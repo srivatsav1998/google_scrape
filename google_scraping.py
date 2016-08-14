bcb8660746e357e1d849c7fb4f8e9d9eb881042d
srivatsav1998 <srivatsav1998@gmail.com>
2016-08-14 11:59:06 +0530

    Add files via upload

---
+++ google_scraping.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+question=raw_input("Enter your query: ")
+site= "http://www.google.com/search?q="+question
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req).read()
+    soup = BeautifulSoup(page)
+    a=soup.findAll('cite',attrs={'class':'_Rm'})
+    for i in a:
+	print 
+        print i.text
+except urllib2.HTTPError, e:
+    print e.fp.read()
